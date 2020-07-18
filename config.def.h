ff4fd8469be355ae8fe9e8a0feee46a734b68e74
Shourai <10200748+Shourai@users.noreply.github.com>
2020-07-18 12:48:28 +0200

    Apply font2

--- config.def.h
+++ config.def.h
@@ -5,7 +5,15 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Liberation Mono:pixelsize=12:antialias=true:autohint=true";
+static char *font = "Jetbrains Mono:pixelsize=14:antialias=true:autohint=true";
+/* Spare fonts */
+static char *font2[] = {
+	"Droid Sans Mono:style=Regular:pixelsize=14:antialias=true:autohint=true",
+	"Menlo:style=Regular:pixelsize=14:antialias=true:autohint=true",
+	"Inconsolata for Powerline:pixelsize=14:antialias=true:autohint=true",
+	"Hack Nerd Font Mono:pixelsize=14:antialias=true:autohint=true", 
+};
+
 static int borderpx = 2;
 
 /*
